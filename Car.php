69a2c3fed6948c83731736ad95a0b7ba08af6102
Thembelani Ngema <thembelaningema20@gmail.com>
2023-12-18 23:11:06 +0200

    Classes,properties,methods
    Classes,properties,methods tutorial

--- Car.php
+++ Car.php
@@ -2,15 +2,44 @@
  
  class Car {
 
-    public $color;
+    private $color = 'none';
     public $weight;
-    public $year;
-    public $make;
+    private $year;
+    private $availableColors = ['red','green','blue','white','yellow'];
+
+    public function setYear($year){
+
+      $this -> year = $year;
+
+    }
+
+    public function getYear(){
+
+      return $this -> year;
+    }
+
+    public function setColor($color){
+
+      if (in_array($color, $this -> availableColors)){
+
+         $this -> color = $color;
+      }
+    }
+
+    public function getColor(){
+
+      return $this -> color;
+    }
 
  }
 
  $myCar = new Car();
- $myCar ->color = 'red';
 
- echo $myCar->color;
+ $myCar ->setColor(color:'red');
+
+ $myCar -> setYear(year:2023);
+
+  echo $myCar -> getColor(). '\n';
+//  echo $myCar -> getYear();
+
 ?> 

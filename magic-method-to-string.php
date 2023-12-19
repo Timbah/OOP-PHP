511a462ced0b2b37be796ffaed6f384a0789c4dc
Thembelani Ngema <thembelaningema20@gmail.com>
2023-12-19 22:08:52 +0200

    magic methods - toString

---
+++ magic-method-to-string.php
@@ -0,0 +1,22 @@
+<?php
+
+  class Person{
+
+    public $name = 'Thembelani';
+    public $surname = 'Ngema';
+    private $phone = '+27662713860';
+
+    public function __toString(){
+
+        //echo "called" . PHP_EOL; 
+        return "Name: $this -> name . Surname: $this -> surname . Phone: $this -> phone . ";
+        
+    }
+
+  }
+
+  $myPerson = new Person();
+
+  echo $myPerson;
+
+?>
